0b5b7dc295855f78fc2bb2a60c3073701fc83812
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-13 12:27:08 -0500

    added cross origin annotation to PostsController

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,14 +1,12 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.Post;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json ")
 public class PostsController {
@@ -17,9 +15,9 @@ public class PostsController {
     @GetMapping
     private List<Post> getAll() {
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "Here's the first post!"));
-        posts.add(new Post(2L, "Post 2", "Here is the second post!"));
-        posts.add(new Post(3L, "Post 3", "Here is the third post!"));
+        posts.add(new Post(1L, "Post 123", "Here's the first post!"));
+        posts.add(new Post(2L, "Post 234", "Here is the second post!"));
+        posts.add(new Post(3L, "Post 345", "Here is the third post!"));
         return posts;
     }
 
