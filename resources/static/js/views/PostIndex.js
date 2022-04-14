49bc4d556014ba4a6d99b28b00484cee1a226b09
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-14 12:10:24 -0500

    refactored form control with bootstrap

--- resources/static/js/views/PostIndex.js
+++ resources/static/js/views/PostIndex.js
@@ -1,12 +1,31 @@
+// TODO: add bootstrap to add pretty
 export default function PostIndex(props) {
+    // language=html;
     return `
         <header>
             <h1>Posts Page</h1>
         </header>
-        <main>
-            <div>
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
-            </div>
-        </main>
+        <div class="container-fluid">
+            <main>
+
+                <div id="posts-container">
+                    ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}
+                </div>
+                <div id="add-post-container">
+                    <div class="mb-3">
+                        <label for="add-post-title" class="form-label">Title</label>
+                        <input class="form-control" id="add-post-title" placeholder="Add title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="add-post-content" class="form-label">Content</label>
+                        <textarea class="form-control" id="add-post-content" rows="3"></textarea>
+                    </div>
+                </div>
+            </main>
+        </div>
     `;
+}
+
+export function PostEvent() {
+
 }
