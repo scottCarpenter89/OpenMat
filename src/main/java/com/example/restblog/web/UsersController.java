be7a0eff6ca1fa8b63dc26419f6b5bfc55945136
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-18 13:50:15 -0500

    added all UserController methods before testing

--- src/main/java/com/example/restblog/web/UsersController.java
+++ src/main/java/com/example/restblog/web/UsersController.java
@@ -1,12 +1,13 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.restblog.data.User.Role.USER;
 
 @CrossOrigin
 @RestController
@@ -17,5 +18,27 @@ public class UsersController {
     private List<User> getAll() {
 
         return null;
+    }
+
+    @GetMapping("{id}")
+    public User getById(@PathVariable Long id) {
+        User user = new User(1, "scottieDon't", "carpenter.scott@rocketmail.com", "butterSc0Tch", LocalDate.now(), USER);
+        System.out.println(user);
+        return user;
+    }
+
+    @PostMapping
+    private void createUser(@RequestBody User user) {
+        System.out.println(user.getId());
+    }
+
+    @PutMapping("{id}")
+    private void updateUser(@PathVariable Long id, @RequestBody User updateUser) {
+        System.out.printf("Update user %s by their id: %d", updateUser, id);
+    }
+
+    @DeleteMapping("{id}")
+    private void deleteUserById(@PathVariable Long id) {
+        System.out.printf("User to be deleted %d", id);
     }
 }
