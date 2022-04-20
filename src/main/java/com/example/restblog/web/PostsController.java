583903792943a956e1a4dabf85f7893d483f45af
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-20 11:32:52 -0500

    PostsRepository was created and PostsController was refactored

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,6 +1,7 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.Post;
+import com.example.restblog.data.PostsRepository;
 import com.example.restblog.data.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,36 +14,51 @@ import static com.example.restblog.data.User.Role.USER;
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json ")
 public class PostsController {
-private static final User user1 = new User(1L, "User 1", "user1@gmail.com", "meow", null, USER, null);
-    private static final User user2 = new User(2L, "User 2", "user2@gmail.com", "ruff", null, USER, null);
+
+    // for dependency injection
+    private PostsRepository postsRepository;
+    public PostsController(PostsRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
+
+//    private static final User user1 = new User(1L, "User 1", "user1@gmail.com", "meow", null, USER, null);
+//    private static final User user2 = new User(2L, "User 2", "user2@gmail.com", "ruff", null, USER, null);
+
     @GetMapping
     private List<Post> getAll() {
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "Here's the first post!", user1));
-        posts.add(new Post(2L, "Post 2", "Here is the second post!", user1));
-        posts.add(new Post(3L, "Post 3", "Here is the third post!", user2));
-        return posts;
+//        ArrayList<Post> posts = new ArrayList<>();
+//        posts.add(new Post(1L, "Post 1", "Here's the first post!", user1));
+//        posts.add(new Post(2L, "Post 2", "Here is the second post!", user1));
+//        posts.add(new Post(3L, "Post 3", "Here is the third post!", user2));
+        return postsRepository.findAll();
     }
 
     @GetMapping("{id}")
     public Post getById(@PathVariable Long id) {
-        Post post = new Post(id , "Post Id: " + id, "Here is the blog post you requested.", user1);
+        Post post = postsRepository.getById(id);
         return post;
     }
 
     @PostMapping
     private void createPost(@RequestBody Post post) {
-        System.out.println(post.getId());
-
+        Post postToAdd = new Post(post.getTitle(), post.getContent());
+        postsRepository.save(postToAdd);
+        System.out.println("A post was created.");
     }
 
     @PutMapping("{id}")
     private void updatePost(@PathVariable Long id, @RequestBody Post editPost){
-        System.out.printf("Backend wants us to update post id %d with %s", id, editPost);
+        Post postToUpdate = postsRepository.getById(id);
+        postToUpdate.setTitle(editPost.getTitle());
+        postToUpdate.setContent(editPost.getContent());
+        postsRepository.save(postToUpdate);
+        System.out.printf("Post with the id, %d was updated.");
     }
 
     @DeleteMapping("{id}")
     private void deletePost(@PathVariable Long id) {
-        System.out.printf("Here is the post id to be deleted %d", id);
+        Post postToDelete = postsRepository.getById(id);
+        postsRepository.delete(postToDelete);
+        System.out.printf("The post with the id %d was deleted.");
     }
 }
--- src/main/java/com/example/restblog/web/UsersController.java
+++ src/main/java/com/example/restblog/web/UsersController.java
@@ -17,24 +17,24 @@ import static com.example.restblog.data.User.Role.USER;
 @RestController
 @RequestMapping(value = "/api/account", headers = "Accept=application/json")
 public class UsersController {
-    private static final Post post1 = new Post(1L, "Post 1", "Here's the first post!", null);
-    private static final Post post2 = new Post(2L, "Post 2", "Here's the second post!", null);
-    private static final Post post3 = new Post(3L, "Post 3", "Here's the third post!", null);
+//    private static final Post post1 = new Post(1L, "Post 1", "Here's the first post!", null);
+//    private static final Post post2 = new Post(2L, "Post 2", "Here's the second post!", null);
+//    private static final Post post3 = new Post(3L, "Post 3", "Here's the third post!", null);
 
     @GetMapping
     private List<User> getAll() {
         ArrayList<User> users = new ArrayList<>();
-        users.add(new User(1, "scottieDon't", "carpenter.scott@rocketmail.com", "butterSc0Tch", LocalDate.now(), USER, Arrays.asList(post1, post2)));
-        users.add(new User(2, "scottieNehPah", "scottieEVIL@gmail.com", "IHaTEU", LocalDate.now(), USER, Arrays.asList(post3)));
+//        users.add(new User(1, "scottieDon't", "carpenter.scott@rocketmail.com", "butterSc0Tch", LocalDate.now(), USER, Arrays.asList(post1, post2)));
+//        users.add(new User(2, "scottieNehPah", "scottieEVIL@gmail.com", "IHaTEU", LocalDate.now(), USER, Arrays.asList(post3)));
         return users;
     }
 
-    @GetMapping("{id}")
-    public User getById(@PathVariable Long id) {
-        User user = new User(id, "scottieDon't", "carpenter.scott@rocketmail.com", "butterSc0Tch", LocalDate.now(), USER, Arrays.asList(post1, post2));
-        System.out.println(user);
-        return user;
-    }
+//    @GetMapping("{id}")
+//    public User getById(@PathVariable Long id) {
+////        User user = new User(id, "scottieDon't", "carpenter.scott@rocketmail.com", "butterSc0Tch", LocalDate.now(), USER, Arrays.asList(post1, post2));
+//        System.out.println(user);
+//        return user;
+//    }
 
     @GetMapping("username")
     public User getByUsername(@RequestParam String username) {
