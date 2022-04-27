ae28b034e2e11b531824e799cb4a17d3e808af7a
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-27 10:52:16 -0500

    attempting to expand card footer to full width of card

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -2,22 +2,37 @@ export default function Login(props) {
     // language=html
     return `
         <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8"/>
-                    <title>Log In</title>
-                </head>
-                <body>
-                    <h1>Log In</h1>
-                    <form id="login-form">
-                        <label for="username">Username</label>
-                        <input id="username" name="username" type="text"/>
-                        <label for="password">Password</label>
-                        <input id="password" name="password" type="password"/>
-                        <input id="login-btn" type="submit" value="Log In"/>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <title>LOG IN</title>
+        </head>
+        <body>
+        <div class="container" id="login-container">
+            <h1>Login</h1>
+            <div class="row">
+                <div class="card container-fluid">
+                    <div class="card-body">
+                        <form id="login-form">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="username" name="username"
+                                       placeholder="name@example.com">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password">
+                            </div>
+                    </div>
+                    <div class="card-footer w-100" id="login-footer">
+                        <input class="btn btn-primary" id="login-btn" type="submit" value="LOG IN"/>
+                    </div>
                     </form>
-                </body>
-            </html>
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
     `;
 }
 
